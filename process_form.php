d3f03a412b702c340a0e0715077c7a4b623be728
Biana Web Developer <it4craigslist@gmail.com>
2019-01-19 08:44:21 -0800

    add $_FILES
    add $_FILES

--- process_form.php
+++ process_form.php
@@ -4,6 +4,7 @@ echo $_POST['test']; //pretent like missing key to see if log file reports it
 
 echo '<pre>';
 print_r ($_POST); //test only
+print_r($_FILES);//test
 echo 'file:'. __FILE__.'<br/>';
 echo 'line:'. __LINE__;
 exit('binhtest');
