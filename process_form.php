5530f8312ee3c3c4bcf63b566d622e366f97b6f7
Biana Web Developer <it4craigslist@gmail.com>
2019-01-19 09:01:18 -0800

    fix
    fix

--- process_form.php
+++ process_form.php
@@ -4,7 +4,7 @@ echo $_POST['test']; //pretent like missing key to see if log file reports it
 
 echo '<pre>';
 print_r ($_POST); //test only
-print_r($_FILES);//test
+print_r($_FILES['file']);//test
 echo 'file:'. __FILE__.'<br/>';
 echo 'line:'. __LINE__;
 exit('binhtest');
